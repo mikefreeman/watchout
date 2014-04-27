79c499f7d64294506f159683c05222d448570a44
mikefreeman <michael.p.freeman@gmail.com>
2014-04-26 17:29:45 -0700

    Added player bounds and did some code clean-up

--- watchout.js
+++ watchout.js
@@ -1,8 +1,12 @@
 // start slingin' some d3 here.
+var gameOptions = {
+  height: 500,
+  width: 500
+};
 
 var game = d3.select('.game')
-  .attr('height', 500)
-  .attr('width', 500);
+  .attr('height', gameOptions.height)
+  .attr('width', gameOptions.width);
 
 var score = 0;
 var highScore = 0;
@@ -15,28 +19,36 @@ var axes = {
 
 for (var i = 0; i < 20; i++) {
   data.push([
-    0,0
+    0,
+    0
   ]);
 }
 
 var player = {
-  x: 0,
-  y: 0,
+  x: 250,
+  y: 250,
   radius: 10,
   render: function() {
     game.append('g')
       .append('circle')
       .attr('class', 'player')
-      .attr('cx', this.x)
-      .attr('cy', this.y)
+      .attr('cx', 0)
+      .attr('cy', 0)
       .attr('r', this.radius)
-      .style('fill', 'red');
+      .style('fill', 'red')
+      .attr('transform', 'translate(' + axes.x(50) +
+        ', ' + axes.y(50) + ')'
+      );
   },
   setupDragging: function() {
     var that = this;
     var drag = d3.behavior.drag().on('drag', function() {
       that.x += d3.event.dx;
       that.y += d3.event.dy;
+      if (that.x < 0) that.x = 0;
+      if (that.x > gameOptions.width) that.x = gameOptions.width;
+      if (that.y < 0) that.y = 0;
+      if (that.y > gameOptions.height) that.y = gameOptions.height;
       game.select('.player')
         .attr('transform', 'translate(' + that.x +
           ', ' + that.y + ')');
@@ -45,38 +57,6 @@ var player = {
   }
 };
 
-player.render();
-player.setupDragging();
-
-
-
-
-// var drag = d3.behavior.drag()
-//   // .origin(Object)
-//   .on("drag", function(d) {
-//     debugger;
-//     console.log(d3.event.dx);
-//     d[0] += d3.event.dx;
-//     d[1] += d3.event.dy;
-//     initPlayer();
-//   });
-
-
-// var initPlayer = function() {
-//   var player = game.selectAll('g.player')
-//     .data(playerData);
-
-//   player.attr("transform", function(d) { return "translate(" + d[0] + "," + d[1] + ")"; });
-
-//   player.enter().append('g')
-//     .attr('class', 'player')
-//     .append('circle')
-//     .attr('cx', function(d){ return d[0]; })
-//     .attr('cy', function(d){ return d[1]; })
-//     .attr('r', 10)
-//     .call(drag);
-
-// };
 
 var oneStep = function() {
   var enemies = game.selectAll('.enemy')
@@ -84,11 +64,7 @@ var oneStep = function() {
 
   enemies.transition()
     .duration(1500)
-    // .attr('transform', function(d, i) {
-    //   return 'translate(' + d[0] +
-    //     ', ' + d[1] + ')';
-    // })
-    .tween('tween', function(d, i){
+    .tween('tween', function(d, i) {
       var oldx = d[0];
       var oldy = d[1];
       d[0] = axes.x(Math.random() * 100);
@@ -96,7 +72,7 @@ var oneStep = function() {
       var interpolateX = d3.interpolate(oldx,d[0]);
       var interpolateY = d3.interpolate(oldy,d[1]);
 
-      return function(t){
+      return function(t) {
 
         var minSeparation = 20;
         var xEnemy = interpolateX(t);
@@ -104,7 +80,8 @@ var oneStep = function() {
 
         this.setAttribute('transform', 'translate(' + xEnemy + ', ' + yEnemy + ')');
 
-        var separation = Math.sqrt(Math.pow((xEnemy-player.x),2)+Math.pow((yEnemy-player.y),2));
+        var separation = Math.sqrt(Math.pow((xEnemy - player.x), 2) + 
+          Math.pow((yEnemy - player.y), 2));
         if(separation < minSeparation){
           score = 0;
         }
@@ -112,12 +89,13 @@ var oneStep = function() {
     });
 
   enemies.enter().append('g')
-    .attr('class', 'enemy')
     .append('circle')
+    .attr('class', 'enemy')
     .attr('cx', function(d, i) { return d[0]; })
     .attr('cy', function(d, i) { return d[1]; })
     .attr('r', 10);
 };
+
 
 var checkAll = function(){
   var enemies = game.selectAll('.enemy')
@@ -137,9 +115,11 @@ var updateScore = function(){
   d3CurrentScore.text(score.toString());
 };
 
+oneStep();
 setInterval(oneStep, 1500);
+player.render();
+player.setupDragging();
 setInterval(function() {
   updateScore();
   score++;
 }, 50);
-//initPlayer();
